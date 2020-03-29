cf1d33dbd9e11b6a79981295865ea0936a7a68e5
ats5515 <36256342+ats5515@users.noreply.github.com>
2020-03-29 19:05:04 +0900

    deploy test4

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ require_once('./template/init.php');
 	<?php
 	require_once('./template/web_header.php');
 	draw_web_header($login_state, $login_user);
-	echo "test3";
+	echo "test4";
 	?>
 	<div class="ats-container">
 		<h1>Marathon Online Judge(仮)</h1>
