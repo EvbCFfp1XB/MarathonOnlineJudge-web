daa15832dc60b2b872b5940f44ff9fd699e57b8c
EvbCFfp1XB <fkjer6atmyvq7ihx@gmail.com>
2022-06-23 19:04:53 +0900

    Change root directory

--- template/useapi.php
+++ template/useapi.php
@@ -2,12 +2,12 @@
 
 function run_cmd($cmd)
 {
-	$shell_user = preg_replace('/\A\/home\/([0-9a-z-]+)\/www.*\z/', '$1', __DIR__);
+	$shell_user = preg_replace('/\A\/home\/([0-9a-z-]+)\/MarathonOnlineJudge-judge\/www.*\z/', '$1', __DIR__);
 	return shell_exec("sudo su -l $shell_user -c '" . $cmd . "'");
 }
 
 function run_cmd_exec($cmd, &$output, &$return_var)
 {
-	$shell_user = preg_replace('/\A\/home\/([0-9a-z-]+)\/www.*\z/', '$1', __DIR__);
+	$shell_user = preg_replace('/\A\/home\/([0-9a-z-]+)\/MarathonOnlineJudge-judge\/www.*\z/', '$1', __DIR__);
 	return exec("sudo su -l $shell_user -c '" . $cmd . "'", $output, $return_var);
 }
